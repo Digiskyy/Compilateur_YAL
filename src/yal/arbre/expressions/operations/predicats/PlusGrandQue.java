d2dfa6ea33a673512956a202aab9a230fa268336
Richer Alexis <alexis.richer1@etu.univ-lorraine.fr>
2020-02-21 18:05:33 +0100

    ajout de plus petit que et plus grand que

--- src/yal/arbre/expressions/operations/predicats/PlusGrandQue.java
+++ src/yal/arbre/expressions/operations/predicats/PlusGrandQue.java
@@ -8,4 +8,12 @@ public class PlusGrandQue extends Operation {
     public PlusGrandQue(Expression gauche, Expression droite) {
         super(gauche, droite);
     }
+
+    @Override
+    public String toMIPS() {
+        return partieGauche.getComparaison1() + partieDroite.getComparaison2()+
+                "\tsgt $t3, $t1, $t2\n"
+                +"\t li $t4, 0\n"
+                +"\tbeq $t3, $t4, Else\n";
+    }
 }
